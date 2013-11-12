1686e7f04cb726c61888ac89c579ca49ed1187cb
bigbiff bigbiff <bigbiff@teamw.in>
2013-11-12 08:00:10 -0500

    fix encryption device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,7 +63,7 @@ TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_SAMSUNG := true
 TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p29"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p25"
 TW_CRYPTO_MNT_POINT := "/data"
 TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic"
 TW_CRYPTO_FS_FLAGS := "0x00000406"
