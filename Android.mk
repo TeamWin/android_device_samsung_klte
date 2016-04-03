3cfddf12fa839eb2faa11fa1602ffd829b258b00
James Christopher Adduono <jc@adduono.com>
2016-04-03 03:59:43 -0400

    Bulldozin'
    Change-Id: I06a4493ddf43d8ed6cddcc61a64d97b86b775cfb

--- Android.mk
+++
@@ -1,3 +0,0 @@
-ifneq ($(filter klte,$(TARGET_DEVICE)),)
-    include $(all-subdir-makefiles)
-endif
--- AndroidProducts.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/omni_klte.mk
